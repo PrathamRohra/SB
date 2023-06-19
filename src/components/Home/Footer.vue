5506da770389203e35ed2c9a8e77f78fa8756936
Pratham Rohra <rohrapratham03@gmail.com>
2023-06-19 12:30:37 +0530

    Fixed Footer

--- src/components/Home/Footer.vue
+++ src/components/Home/Footer.vue
@@ -17,8 +17,8 @@
 
         <!--Our Services-->
         <div class="flex flex-col max-w-[70vw] md:max-w-[140px] mb-10 lg:max-w-lg space-y-1 md:space-y-2">
-            <h1 class="mb-3 underline text-xs decoration-[3px] decoration-[#21B3FF]">Our Services</h1>
-            <p class="text-[8px] font-extralight">Web Development</p>
+            <h1 class="mb-3 underline text-xs xl:text-lg decoration-[3px] decoration-[#21B3FF]">Our Services</h1>
+            <p class="text-[8px] xl:text-sm font-extralight">Web Development</p>
             <p class="text-[8px] font-extralight">Mobile Development</p>
             <p class="text-[8px] font-extralight">UX Designing</p>
             <p class="text-[8px] font-extralight">Cloud Technologies</p>
@@ -26,27 +26,27 @@
 
         <!--Our Links-->
         <div class="flex flex-col max-w-[70vw] md:max-w-[140px] mb-10 lg:max-w-lg space-y-1 md:space-y-2">
-            <h1 class="mb-3 underline text-xs decoration-[3px] decoration-[#21B3FF]">Our Links</h1>
-            <p class="text-[8px] font-extralight">Web Development</p>
-            <p class="text-[8px] font-extralight">Mobile Development</p>
-            <p class="text-[8px] font-extralight">UX Designing</p>
-            <p class="text-[8px] font-extralight">Cloud Technologies</p>
+            <h1 class="mb-3 underline text-xs xl:text-lg decoration-[3px] decoration-[#21B3FF]">Our Links</h1>
+            <p class="text-[8px] xl:text-sm font-extralight">Web Development</p>
+            <p class="text-[8px] xl:text-sm font-extralight">Mobile Development</p>
+            <p class="text-[8px] xl:text-sm font-extralight">UX Designing</p>
+            <p class="text-[8px] xl:text-sm font-extralight">Cloud Technologies</p>
         </div>
 
         <!--Contact US-->
         <div class="flex flex-col max-w-[70vw] md:max-w-[140px] mb-10 lg:max-w-lg space-y-1 md:space-y-2">
-            <h1 class="mb-3 underline text-xs decoration-[3px] decoration-[#21B3FF]">Contact Us</h1>
+            <h1 class="mb-3 underline text-xs xl:text-lg decoration-[3px] decoration-[#21B3FF]">Contact Us</h1>
             <div class="flex flex-row items-center space-x-3">
               <img src="../../assets/Location.svg" class="h-3"/>
-              <p class="text-[8px] font-extralight">Al. Dummyodl 124/23 floor 123 Banswara Street, 02-577 India.</p>
+              <p class="text-[8px] xl:text-sm font-extralight">Al. Dummyodl 124/23 floor 123 Banswara Street, 02-577 India.</p>
             </div>
             <div class="flex flex-row items-center space-x-3">
               <img src="../../assets/Call.svg" class="h-3"/>
-              <p class="text-[8px] font-extralight">00 1800 245 453</p>
+              <p class="text-[8px] xl:text-sm font-extralight">00 1800 245 453</p>
             </div>
             <div class="flex flex-row items-center space-x-3">
               <img src="../../assets/Email.svg" class="h-3"/>
-              <p class="text-[8px] font-extralight">hello@inkyy.com</p>
+              <p class="text-[8px] xl:text-sm font-extralight">hello@inkyy.com</p>
             </div>            
         </div>
     </div>
